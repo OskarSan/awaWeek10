90698aedc6bed853f80a543796b4964bce7fa192
OskarSan <oskar.sankila@gmail.com>
2025-01-22 13:40:56 +0200

    ??

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { changeLanguage } from 'i18next';
-import React, { Suspense } from 'react';
+//import { changeLanguage } from 'i18next';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 
